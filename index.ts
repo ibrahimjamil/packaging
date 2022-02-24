44f192d91fc67da8021ca62d4b7360253ba844e5
ibrahimjamil <ibrahimjamil147@ucp.edu.pk>
2022-02-24 23:54:58 +0500

    added env files

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("hello")
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import { createConnection } from 'typeorm';
+
+
+class Server{
+
+    public start() {
+
+    }
+}
+
+
+const server = new Server();
+server.start();
